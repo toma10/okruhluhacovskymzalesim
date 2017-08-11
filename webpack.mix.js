6df37a9c02ebeb3065c80b93cd59f2335fe7e55b
Tomáš Máčala <tomas.macala@seznam.cz>
2017-08-11 12:59:50 +0200

    Remove video.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,26 +15,22 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .js('resources/assets/js/home.js', 'public/js')
    .sass('resources/assets/sass/app.sass', 'public/css')
    .copy('node_modules/normalize-css/normalize.css', 'public/css/vendor')
-   .copy('node_modules/video.js/dist/video-js.css', 'public/css/vendor')
    .copy('node_modules/dropzone/dist/dropzone.css', 'public/css/vendor')
    .copy('node_modules/lightbox2/dist/css/lightbox.css', 'public/css/vendor')
    .styles([
         'public/css/vendor/normalize.css',
-        'public/css/vendor/video-js.css',
     ], 'public/css/vendor.css')
    .styles([
         'public/css/vendor/lightbox.css',
         'public/css/vendor/dropzone.css',
     ], 'public/css/gallery-vendor.css')
    .copy('node_modules/jquery/dist/jquery.js', 'public/js/vendor')
-   .copy('node_modules/video.js/dist/video.js', 'public/js/vendor')
    .copy('node_modules/dropzone/dist/dropzone.js', 'public/js/vendor')
    .copy('node_modules/bxslider/dist/jquery.bxslider.js', 'public/js/vendor')
    .copy('node_modules/lightbox2/dist/js/lightbox.js', 'public/js/vendor')
    .scripts([
         'public/js/vendor/jquery.js',
         'public/js/vendor/jquery.bxslider.js',
-        'public/js/vendor/video.js',
         'public/js/vendor/noframework.waypoints.min.js',
     ], 'public/js/vendor.js')
    .scripts([
